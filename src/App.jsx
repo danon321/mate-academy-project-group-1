ff1e61de11fd2ed8c50d0f498b28494dca4e108c
Maciej Kujawa <maciekujawa@gmail.com>
2023-12-07 17:17:43 +0100

    add routing and header

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Add from './components/Add/Add';
+import User from './components/User/User';
+import NotFound from './components/NotFound/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <hr />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/user" element={<User />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
