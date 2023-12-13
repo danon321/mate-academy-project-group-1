06f829c01f95d37906d2ca73b7e882b3f5da0d32
Paweł Przybylak <pawel.przybylak21@gmail.com>
2023-12-13 11:40:44 +0100

    adding the createpost component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import './App.scss';
-import './variables/variables.scss';
-
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Add from './components/Add/Add';
+import { Add } from './components/Add/Add';
 import User from './components/User/User';
 import NotFound from './components/NotFound/NotFound';
 import HomeId from './components/HomeId/HomeId';
+import { FormData } from './components/Add/Add';
 
 const App = () => {
+  const handleSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/add" element={<Add />} />
+        <Route path="/add" element={<Add onSubmit={handleSubmit} />} />
         <Route path="/user" element={<User />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
