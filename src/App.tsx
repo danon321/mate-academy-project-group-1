1cc3c030807d4853edb8e4036bd6faaf5dacbe35
Nightycake <michalmarjak@gmail.com>
2023-12-13 12:39:58 +0100

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
-import './variables/variables.scss';
 
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+import { Home } from './components/Home/Home';
 import Add from './components/Add/Add';
 import User from './components/User/User';
 import NotFound from './components/NotFound/NotFound';
@@ -21,7 +20,7 @@ const App = () => {
         <Route path="/user" element={<User />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/posts/:id" element= {<HomeId />} />
+        <Route path="/posts/:id" element={<HomeId />} />
       </Routes>
     </Router>
   );
---
+++ src/InitialPosts/initialPosts.ts
@@ -0,0 +1,58 @@
+import { Post } from '../types/post';
+
+export const initialPosts: Post[] = [
+  {
+    id: 1,
+    title: 'Speed Update - co wiemy o aktualizacji algorytmu Google',
+    content:
+      'Zgodnie z wcześniejszymi zapowiedziami, Google wprowadziłna początku lipca ważne zmiany w algorytmie wyszukiwarki związane z szybkością wczytywania stron. Czego do tej pory dowiedzieliśmy się o tej aktualizacji? Wdrożenie Speed Update, co jest nietypowe dla Google, zostało oficjalnie zapowiedziane z ponad półrocznym wyprzedzeniem na blogu dla webmasterów w styczniu (informowaliśmy o tym w naszym wpisie). Zapowiedziano wtedy, że czas ładowania strony stanie się czynnikiem rankingowym mającym wpływ na jej pozycje w mobilnych wynikach wyszukiwania.',
+    likes: 0,
+    dislikes: 0,
+    date: '20 sierpnia 2018',
+  },
+  {
+    id: 2,
+    title: 'Czynniki rankingowe Google - co ciekawego dowiedzieliśmy się na...',
+    content:
+      'Jakie czynniki są brane pod uwagę przez algorytm Google przy określaniu pozycji stron w wynikach wyszukiwania? Które z nich są najważniejsze, a które mało istotne? Te kwestie zawsze budzą wiele kontrowersji w branży SEO. Oto najciekawsze wnioski, porady, opinie i doświadczenia na temat czynników rankingowych przedstawione na konferencji SMX West w USA. Podczas marcowego Search Marketing Expo (SMX) w ramach sesji „SEO Ranking Factors” swoimi spostrzeżeniami podzielili się m.in. przedstawiciele tak uznanych marek jak SEMrush czy Searchmetrics.',
+    likes: 0,
+    dislikes: 0,
+    date: '2 lipca 2018',
+  },
+  {
+    id: 3,
+    title: 'Google zmienia algorytm - sztuczna inteligencja w służbie obrazu',
+    content:
+      'W poprzedni poniedziałek Google ogłosił kolejne zmiany w swoim algorytmie. Chodzi o wyszukiwanie obrazów, które firma postanowiła usprawnić. Do współpracy zaprzęga sztuczną inteligencję! Eksperci z Google zaczęli pracę nad wyszukiwarką grafiki (Google Images) w 2000 roku, kiedy zauważyli wzmożone zainteresowanie słynną zieloną sukienką Jennifer Lopez z gali nagród Grammy. Dla firmy stało się jasne, że ludzie oczekują od nich nie tylko tekstu, ale też grafiki',
+    likes: 0,
+    dislikes: 0,
+    date: '1 października 2018',
+  },
+  {
+    id: 4,
+    title: 'March Core Update, czyli ciąg dalszy dużych zmian w algorytmie...',
+    content:
+      'Niemal od początku działania wyszukiwarki Google, co pewien czas w algorytmie odpowiedzialnym za dostarczanie rezultatów wyszukiwania wprowadzane są znaczące zmiany. Z taką sytuacją mieliśmy po raz kolejny do czynienia 12/13 marca, gdy na całym świecie odnotowane zostały bardzo duże fluktuacje pozycji stron w wynikach Google. Co zdarza się niezwykle rzadko, tym razem aktualizacja została oficjalnie potwierdzona przez Google i określona mianem „broad core algorithm update”, co oznacza szeroką aktualizację głównego algorytmu wyszukiwarki.',
+    likes: 0,
+    dislikes: 0,
+    date: '12 marca 2020',
+  },
+  {
+    id: 5,
+    title: 'Kolejne zmiany w Google Search Console',
+    content:
+      'W ostatnich dniach w narzędziu Google Search Console (GSC) wprowadzone zostały dość istotne zmiany dotyczące zarówno nowej jak i starej wersji tej aplikacji. 26 listopada zmodyfikowany został raport „Stan indeksu”, który teraz wykorzystuje dane pochodzące z nowego sposobu indeksowania stron „mobile-first indexing” zamiast dotychczasowego indeksu stron w wersji na komputery. Zaktualizowany raport pokazywany jest jednak tylko dla tych witryn, które już wcześniej zostały przeniesione przez Google do nowego indeksu opierającego się na mobilnym wariancie witryny (takie serwisy zostały o tym poinformowane poprzez powiadomienie w GSC).',
+    likes: 0,
+    dislikes: 0,
+    date: '30 listopada 2018',
+  },
+  {
+    id: 6,
+    title: 'Dlaczego kampania SEO nie przynosi efektów?',
+    content:
+      'Uważasz, że pozycjonowanie Twojej strony internetowej (SEO) nie daje zadowalających rezultatów? Jeżeli tak, to czy na pewno wiesz od czego zależy skuteczność działań SEO i w jaki sposób mierzyć ich efekty? Dowiedz się jak miarodajnie ocenić kampanię i co może być przyczyną jej nieefektywności.',
+    likes: 0,
+    dislikes: 0,
+    date: '12 lutego 2019',
+  },
+];
