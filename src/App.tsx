8e27a6cf40ded9bc53056e9193ed87be69336d83
patryk177m <patrykmajewski177@gmail.com>
2023-12-17 18:18:14 +0100

    fix CRLF to LF

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Header from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import User from './components/User/User';
 import NotFound from './components/NotFound/NotFound';
-import HomeId from './components/HomeId/HomeId';
+import { Footer } from './components/footer/Footer';
+import { PostPage } from './components/post';
 
 const App = () => {
   // const handleSubmit = ((data: FormData) => {
@@ -20,8 +21,9 @@ const App = () => {
         <Route path="/user" element={<User />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/posts/:id" element={<HomeId />} />
+        <Route path="/posts/:id" element={<PostPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <span>Contact the Publisher</span>
+      <span>Explorate</span>
+      <span>Headquarter</span>
+      <span>Connections</span>
+    </footer>
+  );
+};
