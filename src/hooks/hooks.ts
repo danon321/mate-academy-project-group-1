f31d0023e216a5b6f9686ed8e60f459b9cad931c
Nightycake <michalmarjak@gmail.com>
2023-12-28 11:44:55 +0100

    remove unnecessary files & moving some of them for redux

--- src/hooks/hooks.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { RootState } from '../app/store';
-
-export const usePostSelector: TypedUseSelectorHook<RootState> = useSelector;
