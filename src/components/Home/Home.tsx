9637ce439689c1a76cda425e56aed82a3130f1ca
patryk177m <patrykmajewski177@gmail.com>
2024-01-09 11:10:11 +0100

    appearance of a single post on the Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -13,19 +13,46 @@ export const Home: React.FC = () => {
     <div className="container">
       <div className="post-grid">
         {initialPosts.map((post) => (
-          <div key={post.id} className="post">
-            <div className="post-header">
-              <p>{post.date}</p>
-              <h2>{post.title}</h2>
+          <div key={post.id} className="post-container">
+            <div className="post-image">
+              <img
+                className="post-image image"
+                src="/static/images/posts/postImage1.svg"
+                alt="image"
+              />
             </div>
-            <p>{limitContent(post.content, 120)}</p>
-            <a href={`/posts/${post.id}`} className="read-more">
-              Czytaj więcej...
-            </a>
-            <div className="like-dislike-container">
+            <div className="post-section">
+              <div className="post-date">
+                <p>{post.date}</p>
+              </div>
+              <div className="post-title">
+                <h2>{limitContent(post.title, 50)}</h2>
+              </div>
+              <div className="post-description">
+                <p className="post-text">{limitContent(post.content, 110)}</p>
+                <a href={`/posts/${post.id}`} className="read-more">
+                  Czytaj więcej...
+                </a>
+              </div>
+              <div className="avatar-container">
+                <div className='avatar-img'>
+                  <img
+                    className="img"
+                    src="/static/images/posts/fotoAvatar.svg"
+                    alt='P'
+                  />
+                </div>
+
+                <div>
+                  <h5 className="avatar-name">Avatar name</h5>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="like-dislike-container">
               <div className="like-counter">👍 {post.likes}</div>
               <div className="dislike-counter">👎 {post.dislikes}</div>
-            </div>
+            </div> */}
           </div>
         ))}
       </div>
