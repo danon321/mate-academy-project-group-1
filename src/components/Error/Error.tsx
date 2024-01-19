45ca407f94fd70456c32562e099862bedbce0ecf
Nightycake <michalmarjak@gmail.com>
2024-01-19 11:16:56 +0100

    moving error & skeleton files

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -6,7 +6,7 @@ export const Error = () => {
   // stop = &#9940;
   return (
     <div className="container">
-      <div className='error'>
+      <div className="error">
         <div className="error-container">
           <ReportGmailerrorredIcon sx={{ color: '#d50000', fontSize: 240 }} />
           <h3 className="error-description">Coś poszło nie tak.</h3>
