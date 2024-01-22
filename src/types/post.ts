0d54889bd630773286f762a05813fdb6457508c0
patryk177m <patrykmajewski177@gmail.com>
2024-01-22 15:11:34 +0100

    add post.ts

---
+++ src/types/post.ts
@@ -0,0 +1,14 @@
+import { User } from "./user";
+
+
+export type Post = {
+  id: number;
+  title: string;
+  content: string;
+  likes: number;
+  dislikes: number;
+  date: Date;
+  image: string | null;
+  // userId: number;
+  user: User;
+};
