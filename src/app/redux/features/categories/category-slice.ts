6bb45cec91cc3ab8337eaafec15430164bbda483
patryk177m <patrykmajewski177@gmail.com>
2024-02-06 08:55:08 +0100

    category support in redux

---
+++ src/app/redux/features/categories/category-slice.ts
@@ -0,0 +1,36 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Category } from '../../../../types/category';
+import { fetchCategories } from '../../../../api/services/fetchPost';
+
+type State = {
+  isLoading: boolean;
+  error: boolean;
+  categories: Category[];
+}
+
+const initialState: State = {
+  isLoading: true,
+  error: false,
+  categories: [],
+};
+
+export const categorySlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategories.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(
+      fetchCategories.fulfilled,
+      (state, action: PayloadAction<Category[]>) => {
+        state.categories = action.payload;
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(fetchCategories.rejected, (state) => {
+      state.error = true;
+    });
+  },
+});
