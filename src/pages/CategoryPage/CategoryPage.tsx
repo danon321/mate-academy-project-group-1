eb58fca1c43bb17ce3d652b6a4b950702febba73
Maciej Kujawa <maciekujawa@gmail.com>
2024-02-09 08:48:58 +0100

    add category header two times

---
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -0,0 +1,16 @@
+import './categoryPage.scss';
+
+export const CategoryPage = () => {
+  return (
+    <>
+      <div className="category-page">
+        <div className="category__picture">
+          <div className="category__picture-textbox">
+            <h1 className="category__picture-title">Nazwa kategorii</h1>
+          </div>
+        </div>
+        <div className="container"></div>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/CategoryPage2/CategoryPage2.tsx
@@ -0,0 +1,17 @@
+import './categoryPage2.scss';
+
+export const CategoryPage2 = () => {
+  return (
+    <>
+      <div className="slide">
+        <div className="container">
+          <div className="slide__overlay">
+            <div className="slide__overlay__category">Kategoria I</div>
+            <div className="slide__overlay__title">Nazwa kategorii</div>
+            <div className="slide__overlay__text">opis kategorii w zależności od potrzeb</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
