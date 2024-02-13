e0f9c9967d5715a8262884660dcc9aaf8dd93ce1
Maciej Kujawa <maciekujawa@gmail.com>
2024-02-13 09:51:12 +0100

    category header add changes in App.tsx

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -3,13 +3,15 @@ import './categoryPage.scss';
 export const CategoryPage = () => {
   return (
     <>
-      <div className="category-page">
-        <div className="category__picture">
-          <div className="category__picture-textbox">
-            <h1 className="category__picture-title">Nazwa kategorii</h1>
+      <div className="slide">
+        <div className="container">
+          <div className="slide__overlay">
+            <div className="slide__overlay__title">Nazwa kategorii</div>
+            <div className="slide__overlay__text">
+              opis kategorii w zależności od potrzeb
+            </div>
           </div>
         </div>
-        <div className="container"></div>
       </div>
     </>
   );
