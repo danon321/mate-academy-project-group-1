afc07da6e6ce4853bcff93d2d277be71ebd905d9
Maciej Kujawa <maciekujawa@gmail.com>
2024-02-13 09:36:02 +0100

    category header delete div

--- src/pages/CategoryPage2/CategoryPage2.tsx
+++ src/pages/CategoryPage2/CategoryPage2.tsx
@@ -6,9 +6,10 @@ export const CategoryPage2 = () => {
       <div className="slide">
         <div className="container">
           <div className="slide__overlay">
-            <div className="slide__overlay__category">Kategoria I</div>
             <div className="slide__overlay__title">Nazwa kategorii</div>
-            <div className="slide__overlay__text">opis kategorii w zależności od potrzeb</div>
+            <div className="slide__overlay__text">
+              opis kategorii w zależności od potrzeb
+            </div>
           </div>
         </div>
       </div>
